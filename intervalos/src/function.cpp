fb95f31d2965993a92ea9267ae90fa13e7880869
Gabriel Alexsandro <gabrielalexsandro2011@gmail.com>
2021-06-27 16:54:30 -0300

    intervalos 2

---
+++ intervalos/src/function.cpp
@@ -0,0 +1,36 @@
+#include "function.h"
+void func()
+{
+  int x, i1=0, i2=0, i3=0, i4=0, fora=0;
+  while( cin >> std::ws >> x) {
+    // realização da contagem em relação aos intervalos
+    if(x >= 0 && x < 25)
+    {
+      i1++;
+    }
+    else if(x >= 25 && x < 50)
+    {
+      i2++;
+    }
+    else if(x >= 50 && x < 75)
+    {
+      i3++;
+    }
+    else if(x >= 75 && x < 100)
+    {
+      i4++;
+    }
+    else
+    {
+      fora++;
+    }
+  }
+  
+  //saida das porcentagens
+  setprecision(4);
+  cout<<(float)100*i1/(i1+i2+i3+i4+fora)<<endl;
+  cout<<(float)100*i2/(i1+i2+i3+i4+fora)<<endl;
+  cout<<(float)100*i3/(i1+i2+i3+i4+fora)<<endl;
+  cout<<(float)100*i4/(i1+i2+i3+i4+fora)<<endl;
+  cout<<(float)100*fora/(i1+i2+i3+i4+fora)<<endl;
+}
---
+++ intervalos/src/function.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <iomanip>
+using std::setprecision;
+
+void func();
+
+#endif
