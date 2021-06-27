1439a75817476581b1b08e3b0c2cdb33e26c8e85
Gabriel Alexsandro <gabrielalexsandro2011@gmail.com>
2021-06-27 16:29:27 -0300

    intervalos

--- autor.md
+++ autor.md
@@ -10,7 +10,7 @@ Preencha os dados abaixo para identificar a autoria do trabalho.
 
 - [ ] Negativos 5
 - [ ] Soma Vizinhos
-- [ ] Intervalos
+- [x] Intervalos
 - [x] Fibonacci
 - [ ] Minmax
 - [ ] Inverter
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*!
+ * @brief This code implements the Intervalos programming problem
+ * @author selan
+ * @data June, 6th 2021
+ */
+#include "function.h"
+
+int main(void)
+{
+    // porcentagens();
+    func();
+
+    return 0;
+}
