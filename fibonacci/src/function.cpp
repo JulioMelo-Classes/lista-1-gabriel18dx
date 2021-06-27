29fa1dff4a53ca68273e5faf34ec6ab57e1df56c
Gabriel Alexsandro <gabrielalexsandro2011@gmail.com>
2021-06-27 13:46:03 -0300

    fibonacci

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,18 @@
 #include "function.h"
+#include <iostream>
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    // TODO: adicione o seu código aqui.
+    std::vector<unsigned int> v;
+    int n1 = 0, n2 = 1, aux = 0;
 
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    while(n2 < n)
+    {
+      v.push_back(n2);
+      aux = n2;
+      n2 = n1 + n2;
+      n1 = aux;
+    }
+
+    return v;
 }
